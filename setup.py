7cf5f60f26e19b624f7482a5ac4e65621eb28885
Hugh Sorby <h.sorby@auckland.ac.nz>
2016-08-01 22:48:31 +1200

    Corrections to setup.py

--- setup.py
+++ setup.py
@@ -1,23 +1,35 @@
 from setuptools import setup, find_packages
-import sys, os
+import sys, os, io
 
-# The dependencies variable is used by MAP Client to
-# determine if further downloads are required.  Please
-# list all dependencies here.
-dependencies = [] # Insert plugin dependencies here
+# List all of your Python package dependencies in the
+# requirements.txt file
 
-setup(name=u'mapclientplugins.smoothfitstep',
-      version='0.0',
-      description='',
-      long_description="",
-      classifiers=[],
-      author=u'Richard Christie',
-      author_email='',
-      url='',
-      license='GPL',
-      packages=find_packages(exclude=['ez_setup',]),
-      namespace_packages=['mapclientplugins'],
-      include_package_data=True,
-      zip_safe=False,
-      install_requires=dependencies,
-      )
+def readfile(filename, split=False):
+    with io.open(filename, encoding="utf-8") as stream:
+        if split:
+            return stream.read().split("\\n")
+        return stream.read()
+
+readme = readfile("README.rst", split=True)[3:]  # skip title
+requires = readfile("requirements.txt", split=True)
+license = readfile("LICENSE")
+
+setup(
+    name=u'mapclientplugins.smoothfitstep',
+    version='0.1.0',
+    description='',
+    long_description='\n'.join(readme) + license,
+    classifiers=[
+      "Development Status :: 3 - Alpha",
+      "Programming Language :: Python",
+    ],
+    author=u'Richard Christie',
+    author_email='',
+    url='',
+    license='GPL',
+    packages=find_packages(exclude=['ez_setup',]),
+    namespace_packages=['mapclientplugins'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=requires,
+    )
